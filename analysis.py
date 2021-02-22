a811903ed036992e40c2a3524d9136cb787f4d7d
Emma Baker <emma.baker@wustl.edu>
2021-02-22 14:44:00 -0600

    Analysis done

---
+++ analysis.py
@@ -0,0 +1,24 @@
+import pandas as pd
+from scipy import stats
+from sklearn.cluster import KMeans
+from sklearn.decomposition import PCA
+import seaborn as sns
+import matplotlib as mpl
+mpl.use('MacOSX')
+import numpy as np
+import matplotlib.pyplot as plt
+
+data = pd.read_csv('data/articles.csv')
+# plt.scatter(data['Article'], data['duration'])
+# plt.xlabel("Article")
+# plt.ylabel("Duration")
+# plt.show()
+
+kmeans = KMeans(n_clusters=47, random_state=1).fit(data)
+predicted_data=data.copy()
+predicted_data['pred'] = kmeans.fit_predict(data)
+
+plt.scatter(predicted_data["Article"], predicted_data['duration'], c=predicted_data['pred'], cmap='Blues')
+plt.xlabel("Article")
+plt.ylabel("Duration")
+plt.show()
